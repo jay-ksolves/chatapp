58e817807a7c675ba9b7874a839894ed5e286011
jay-ksloves <jay.prakash@ksolves.com>
2023-10-19 13:35:02 +0530

    done validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates_uniqueness_of :username, presence: true
-  validates_presence_of :email
+  validates :username, presence: true, format: { with: /\A[a-zA-Z0-9_\s.']+\z/ }, uniqueness: { case_sensitive: false }
+  # validates_presence_of :email
+  validates_uniqueness_of :email, presence: true
+
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
   has_many :messages, dependent: :destroy
